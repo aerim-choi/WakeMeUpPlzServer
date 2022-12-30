fcce86e9d83b48b85bff80c17a4628997731289e
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2022-12-30 23:08:05 +0900

    User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict"
 
-const UserStorage =require("../../models/UserStorage");
+const User =require("../../models/User");
 const output ={
     home : (req,res)=>{
         res.render("home/index");
@@ -14,20 +14,8 @@ const output ={
 //로그인 인증 process
 const process={
     login:(req,res)=>{
-        const id =req.body.id;
-        const psword = req.body.psword; 
-        
-        const users = UserStorage.getUsers("id","psword");
-        const response = {};
-        if(users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if(users.psword[idx]===psword){
-                response.sucess=true;
-                return res.json();
-            }
-        }
-        response.sucess=false;
-        response.msg="로그인에 실패하셨습니다.";
+        const user= new User(req.body);
+        const response=user.login();
         return res.json(response);
     },
 };
