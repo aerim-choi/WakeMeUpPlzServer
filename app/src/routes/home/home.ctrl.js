70b4fe083dfb99284f1d8443ea345531d8c64a1e
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2022-12-31 13:39:34 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -16,9 +16,9 @@ const output ={
 
 //로그인 인증 process
 const process={
-    login:(req,res)=>{
+    login:async (req,res)=>{
         const user= new User(req.body);
-        const response=user.login();
+        const response=await user.login();
         return res.json(response);
     },
     register:(req,res)=>{
