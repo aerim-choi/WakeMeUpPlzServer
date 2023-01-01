d863601fa5ee473dac5906f3e4fbbe467814af7a
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-01-01 17:29:56 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -8,6 +8,7 @@ class User{
 
     async login(){
         const client =this.body
+        console.log(`login => client : ${client.id},${client.psword}`);
         const {id,psword}= await UserStorage.getUserInfo(client.id);
         if(id){
             if(id===client.id && psword ===  client.psword){
@@ -18,10 +19,14 @@ class User{
         return {success:false,msg:"존재하지 않는 아이디입니다."};
         
     }
-    register(){
+    async register(){
         const client = this.body;
-        const response = UserStorage.save(client);
-        return response;
+        try{
+            const response = await UserStorage.save(client);
+            return response;
+        }catch(err){
+            return {success : false, msg:err};
+        }
     }
 
 
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,46 +1,57 @@
 "use strict"
 
-const fs =require("fs").promises;
+const fs = require("fs").promises;
 
-class UserStorage{
-    static #getUserInfo(data,id){
-        const users= JSON.parse(data);
-        const idx=users.id.indexOf(id);
-        const usersKeys=Object.keys(users); //=>[id, psword, name]
-        const userInfo = usersKeys.reduce((newUser,info) => {
-        newUser[info]=users[info][idx];
-        return newUser;
-        },{});
+class UserStorage {
+    static #getUserInfo(data, id) {
+        const users = JSON.parse(data);
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users); //=>[id, psword, name]
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
         return userInfo;
     }
-    
-    static getUsers(...fields){
-        // const users = this.#users;
-        const newUsers = fields.reduce((newUsers,field)=>{
-            if(users.hasOwnProperty(field)){
-                newUsers[field]=users[field];
+    static #getUsers(data, isALL, fields) {
+        const users = JSON.parse(data);
+        if (isALL) return users;
+        const newUsers = fields.reduce((newUsers, field) => {
+            if (users.hasOwnProperty(field)) {
+                newUsers[field] = users[field];
             }
             return newUsers;
-        },{});   
+        }, {});
         return newUsers;
     }
-
-    static getUserInfo(id){
+    static getUsers(isALL, ...fields) {
         return fs.readFile("./src/databases/users.json")
-        .then((data)=>{
-            return this.#getUserInfo(data,id);
-        })
-        .catch((err)=>console.error);
+            .then((data) => {
+                return this.#getUsers(data, isALL, fields);
+            })
+            .catch((err) => console.error);
     }
 
-    static save(userInfo){
-        // const users=this.#users;
+    static getUserInfo(id) {
+        return fs.readFile("./src/databases/users.json")
+            .then((data) => {
+                return this.#getUserInfo(data, id);
+            })
+            .catch((err) => console.error);
+    }
+
+    static async save(userInfo) {
+        const users = await this.getUsers(true);
+        if (users.id.includes(userInfo.id)) {
+            throw "이미 존재하는 아이디입니다.";
+        }
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
-        return {success: true}
-
+        //데이터 추가 
+        fs.writeFile("./src/databases/users.json", JSON.stringify(users));
+        return { success: true }
     }
 
 }
-module.exports=UserStorage;
+module.exports = UserStorage;
