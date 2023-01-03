504cff89bd51432e3e3664d5f5edac704ded7e30
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-01-03 14:22:06 +0900

    mysql1 version

--- app/src/models/AlarmStorage.js
+++ app/src/models/AlarmStorage.js
@@ -1,30 +1,60 @@
 "use strict"
+const e = require("express");
 const db=require("../config/db");
 /*
 AlarmStorage에서는 DB를 CRUD(생성,읽기,수정,삭제)역할
 */
 class AlarmStorage {
-
-    static getAlarmInfo(id) {
+    static dataList;
+    static async getDataList(){
+        console.log("2");  
+        return this.dataList;
+         
+    }
+    static async getAlarmInfo(id) {
         return new Promise((resolve,reject)=>{
-            const query = "SELECT * FROM alarms WHERE id =?;";
+            const query = "select alarmNum,title,helper,dorN,isActivated,isHelperActivated,ringTone,id,dates from alarms where id=?;";
+            const query2 = "select Sun,Mon,Tue,Wed,Thur,Fri,Sat from dates  where alarmNum=?;";
+            
+            this.dataList=[];
             db.query(query,[id],(err,data)=>{
                 if(err)reject(`${err}`);
-                console.log(data);
-                resolve(data);
+
+                for(var element of data){
+                    console.log(element);
+                    db.query(query2,[element.alarmNum],(err,data2)=>{
+                        if(err)reject(`${err}`);
+                        element.dates=data2[0];
+                        console.log(element);
+                        this.dataList.push(element);
+                        
+                    });
+                }
+
             });
+            console.log("1");  
+            resolve(this.getDataList());
+            console.log("4");        
         });
         
     }
+    
+
 
     static async alarmSave(alarmInfo) {
         return new Promise((resolve,reject)=>{
-            const query = "INSERT INTO alarms(alarmNum,alarmTime, alarmDay,title,sound,helper,id) VALUES(?,?,?,?,?,?,?);" ;
+            const query = "INSERT INTO alarms(alarmNum,title, helper,dorN,isActivited,isHelperActivated,ringTone,alarmTime,id) VALUES(?,?,?,?,?,?,?,?,?);" ;
+            const query2 = "INSERT INTO dates(Sun,Mon,Tue,Wed,Thur,Fri,Sat,alarmNum) VALUES(?,?,?,?,?,?,?,?)";
             db.query(query,
-                [alarmInfo.alarmNum,alarmInfo.alarmTime,alarmInfo.alarmDay,alarmInfo.title,alarmInfo.sound,alarmInfo.helper,alarmInfo.id],
+                [alarmInfo.alarmNum,alarmInfo.title,alarmInfo.helper,alarmInfo.dorN,alarmInfo.isActivited,alarmInfo.isHelperActivated,alarmInfo.ringTone,alarmInfo.alarmTime,alarmInfo.id],
                 (err)=>{
                 if(err)reject(`${err}`);
                 resolve ({sucess:true});
+            });
+            db.query2(query2,[alarmInfo.Sun,alarmInfo.Mon,alarmInfo.Tue,alarmInfo.Wed,alarmInfo.Thur,alarmInfo.Sat,alarmInfo.alarmNum],
+                (err)=>{
+                    if(err)reject(`${err}`);
+                    resolve({sucess:true});
             });
         });
     }
