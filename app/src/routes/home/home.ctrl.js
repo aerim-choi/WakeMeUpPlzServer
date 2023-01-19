fd48d9f4367de29113712769c2113e40b2e1213c
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-01-19 16:09:05 +0900

    최종 최적화

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -5,15 +5,15 @@ const Alarm = require("../../models/Alarm");
 const User =require("../../models/User");
 const output ={
     home : (req,res)=>{
-        logger.info(`GET /200 "홈 화면으로 이동"`);
+        logger.info(`GET /304 "홈 화면으로 이동"`);
         res.render("home/index");
     },
     login : (req,res)=>{
-        logger.info(`GET /200 "로그인 화면으로 이동"`);
+        logger.info(`GET /304 "로그인 화면으로 이동"`);
         res.render("home/login");
     },
     register:(req,res)=>{
-        logger.info(`GET /200 "회원가입 화면으로 이동"`);
+        logger.info(`GET /304 "회원가입 화면으로 이동"`);
         res.render("home/register");
     },
     alarm:(req,res)=>{
@@ -35,30 +35,24 @@ const process={
     login:async (req,res)=>{
         const user= new User(req.body);
         const response=await user.login();
-        if(response.err){
-            logger.error(
-                `POST /login 200 Response: "success: ${response.success},${response.err}"`
-            )
-        }
-        else
-            logger.info(
-                `POST /login 200 Response: "success: ${response.success},msg:${response.msg}"`
-            );
-        return res.json(response);
+        const url ={
+            method:"POST",
+            path:"/login",
+            status: response.arr?400:200,
+        };
+        log(response,url);
+        return res.status(url.status).json(response);
     },
     register:async (req,res)=>{
         const user= new User(req.body);
         const response=await user.register();
-        if(response.err){
-            logger.error(
-                `POST /login 200 Response: "success: ${response.success},${response.err}"`
-            )
-        }
-        else
-            logger.info(
-                `POST /register 200 Response: "success: ${response.success},msg:${response.msg}"`
-            );
-        return res.json(response);
+        const url ={
+            method:"POST",
+            path:"/register",
+            status: response.arr?409:201,
+        };
+        log(response,url);
+        return res.status(url.status).json(response);
     },
     getAlarm:async(req,res)=>{
         const alarm= new Alarm(req.body);
@@ -84,6 +78,17 @@ const process={
 
 
 };
+const log =(response,url)=>{
+    if(response.err){
+        logger.error(
+            `${url.method} ${url.path} ${url.status} Response: ${response.success},${response.err}"`
+        )
+    }
+    else
+        logger.info(
+            `${url.method} ${url.path} ${url.status} Response: ${response.success} ${response.msg||""}"`
+        );
+}
 
 module.exports={
     output,
